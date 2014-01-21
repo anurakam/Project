1b4f50b78b9b04ac64275ae950cbcf683e7b5d15
anurakam <lovemaijing@gmail.com>
2014-01-21 23:28:35 +0700

    calshare

--- RubberThailand/src/com/example/rubberthailand/ViewCalShare.java
+++ RubberThailand/src/com/example/rubberthailand/ViewCalShare.java
@@ -2,13 +2,52 @@ package com.example.rubberthailand;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ViewCalShare extends Activity{
+	private EditText editText1;
+	private EditText EditText01;
+	private Button cal_btn;
+	private Button reset_cal_btn;
+	private TextView textView1;
+	private String value,rate,total;
+	private double cal;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.cal_share);
 		
+		editText1 = (EditText) findViewById(R.id.editText1);
+		EditText01 = (EditText) findViewById(R.id.EditText01);
+		cal_btn = (Button) findViewById(R.id.cal_btn);
+		reset_cal_btn = (Button) findViewById(R.id.reset_cal_btn);
+		textView1 = (TextView) findViewById(R.id.textView1);
+		cal_btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				value = editText1.getText().toString();
+				double v = Double.parseDouble(value);
+				rate = EditText01.getText().toString();
+				double r = Double.parseDouble(rate);
+				cal = v * (r/100);
+				total = String.valueOf(cal);
+				textView1.setText(total);
+			}
+		});
+		reset_cal_btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				editText1.setText("");
+				EditText01.setText("");
+				textView1.setText("");
+			}
+		});
 		
 	}
 }
--- RubberThailand/src/com/example/rubberthailand/ViewPriceToday.java
+++ RubberThailand/src/com/example/rubberthailand/ViewPriceToday.java
@@ -9,24 +9,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
 public class ViewPriceToday extends Activity {
 	private EditText editText1;
 	private EditText EditText01;
-	private Button button1;
+	private Button cal_price_btn;
+	private Button reset_price_btn;
 	private TextView textView8;
-	private String rubberprice,quantity,total;
+	private String rubberprice, quantity, total;
 	private double cal;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.price_today);
 		editText1 = (EditText) findViewById(R.id.editText1);
 		EditText01 = (EditText) findViewById(R.id.EditText01);
-		button1 = (Button) findViewById(R.id.button1);
+		cal_price_btn = (Button) findViewById(R.id.cal_price_btn);
+		reset_price_btn = (Button) findViewById(R.id.reset_price_btn);
 		textView8 = (TextView) findViewById(R.id.textView8);
-		button1.setOnClickListener(new OnClickListener() {
-			
+		cal_price_btn.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View arg0) {
 				rubberprice = editText1.getText().toString();
@@ -36,6 +38,15 @@ public class ViewPriceToday extends Activity {
 				cal = rp + qt;
 				total = String.valueOf(cal);
 				textView8.setText(total);
+			}
+		});
+		reset_price_btn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				editText1.setText("");
+				EditText01.setText("");
+				textView8.setText("");
 			}
 		});
 	}
